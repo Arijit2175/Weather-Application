ce400dbd2e8e43552d09f6fdc3b5b25e1805cc1d
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-19 18:41:08 +0530

    initialized the function

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const location_not_found = document.querySelector('.location-not-found');
 
 const weather_body = document.querySelector('.weather-body');
 
+async function checkWeather(city){
+    const api_key = "";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
