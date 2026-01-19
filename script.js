436cb2147a8e895bf80f7914eea407f05fc5c6bc
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-19 18:47:49 +0530

    added api for the weather data

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const location_not_found = document.querySelector('.location-not-found');
 const weather_body = document.querySelector('.weather-body');
 
 async function checkWeather(city){
-    const api_key = "";
+    const api_key = "API_KEY";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
